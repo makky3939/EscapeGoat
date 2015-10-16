70a6767cd2abd30b455dbb491de92a4ee60bbc3e
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2015-10-16 17:29:30 +0900

    delete nnecessary methods

--- src/actions/RecordAction.js
+++ src/actions/RecordAction.js
@@ -4,79 +4,21 @@ import RecordConstants from './../constants/RecordConstants'
 let RecordActions = {
 
   /**
-   * @param  {string} text
+   * @param  {object} record
    */
-  create: function(text) {
+  create: function(record) {
     AppDispatcher.dispatch({
       actionType: RecordConstants.RECORD_CREATE,
-      text: text
+      record: record
     })
   },
-
-  /**
-   * @param  {string} id The ID of the ToDo item
-   * @param  {string} text
-   */
-  updateText: function(id, text) {
-    AppDispatcher.dispatch({
-      actionType: RecordConstants.RECORD_UPDATE_TEXT,
-      id: id,
-      text: text
-    })
-  },
-
-  /**
-   * Toggle whether a single ToDo is complete
-   * @param  {object} todo
-   */
-  toggleComplete: function(todo) {
-    let id = todo.id
-    let actionType = todo.complete ?
-        RecordConstants.RECORD_UNDO_COMPLETE :
-        RecordConstants.RECORD_COMPLETE
-
-    AppDispatcher.dispatch({
-      actionType: actionType,
-      id: id
-    })
-  },
-
-  /**
-   * Mark all ToDos as complete
-   */
-  toggleCompleteAll: function() {
-    AppDispatcher.dispatch({
-      actionType: RecordConstants.RECORD_TOGGLE_COMPLETE_ALL
-    })
-  },
-
-  /**
-   * @param  {string} id
-   */
-  destroy: function(id) {
-    AppDispatcher.dispatch({
-      actionType: RecordConstants.RECORD_DESTROY,
-      id: id
-    })
-  },
-
   /**
    */
   destroyAll: function() {
     AppDispatcher.dispatch({
       actionType: RecordConstants.RECORD_DESTROY_ALL
     })
-  },
-
-  /**
-   * Delete all the completed ToDos
-   */
-  destroyCompleted: function() {
-    AppDispatcher.dispatch({
-      actionType: RecordConstants.RECORD_DESTROY_COMPLETED
-    })
   }
-
 };
 
 module.exports = RecordActions
--- src/constants/RecordConstants.js
+++ src/constants/RecordConstants.js
@@ -2,11 +2,5 @@ import keyMirror from 'keymirror'
 
 module.exports = keyMirror({
   RECORD_CREATE: null,
-  RECORD_DESTROY_ALL: null,
-  RECORD_COMPLETE: null,
-  RECORD_DESTROY: null,
-  RECORD_DESTROY_COMPLETED: null,
-  RECORD_TOGGLE_COMPLETE_ALL: null,
-  RECORD_UNDO_COMPLETE: null,
-  RECORD_UPDATE_TEXT: null
+  RECORD_DESTROY_ALL: null
 })
