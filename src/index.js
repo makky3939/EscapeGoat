876d65fdb45691281881e2a72b79229324a0d465
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2015-10-25 01:06:56 +0900

    update path

--- src/index.js
+++ src/index.js
@@ -21,10 +21,10 @@ onUpdateHandler()
 React.render((
   <Router onUpdate={() => onUpdateHandler()} history={createBrowserHistory()}>
     <Route path="escapegoat/" component={Layout}>
-      <Route path="escapegoat/usage" component={Usage} />
-      <Route path="escapegoat/records" component={Records} />
-      <Route path="escapegoat/dashboard" component={Dashboard} />
-      <Route path="escapegoat/about" component={About} />
+      <Route path="usage" component={Usage} />
+      <Route path="records" component={Records} />
+      <Route path="dashboard" component={Dashboard} />
+      <Route path="about" component={About} />
     </Route>
     <Redirect from="escapegoat/" to="escapegoat/usage" />
   </Router>
