bffa78869f552dc740ba7d2089b01cb15a3b444e
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2015-10-25 13:42:41 +0900

    fix text

--- src/components/project/Footer.js
+++ src/components/project/Footer.js
@@ -11,16 +11,16 @@ const Footer = React.createClass({
           <p>
             <ol className="list-inline">
               <li>
-                <a href="/escapegoat/">Top</a>
-              </li>
-              <li>
                 <a href="https://github.com/makky3939/EscapeGoat" target="_blank">Github Project</a>
               </li>
               <li>
                 <a href="https://github.com/makky3939/EscapeGoat/issues" target="_blank">Issues</a>
               </li>
               <li>
-                <a href="http://www.makky.io" target="_blank">www.makky.io</a>
+                <a href="http://www.makky.io" target="_blank">makky.io</a>
+              </li>
+              <li>
+                <a href="https://twitter.com/share?url=http://app.makky.io/escapegoat&hashtags=klis&text=klis向け卒業判定アプリ EscapeGoat" target="_blank">Share on Twitter</a>
               </li>
             </ol>
             <span>&copy; makky.io All Rights Reserved.<br /></span>
