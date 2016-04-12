cc55007426df393f2e9716591b8b158e3483cf19
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2016-04-12 12:46:16 +0900

    label -> tag

--- src/components/ui/Score.js
+++ src/components/ui/Score.js
@@ -7,14 +7,14 @@ var Score = React.createClass({
   render: function() {
     switch(this.props.status) {
       case "none":
-        return <span className='label label-warning'>未履修</span>;
+        return <span className='tag tag-warning'>未履修</span>;
         break;
 
       case "wip":
-        return <span className='label label-info'>履修中</span>;
+        return <span className='tag tag-info'>履修中</span>;
         break;
       default:
-        return <span className='label label-success'>{this.props.status}</span>;
+        return <span className='tag tag-success'>{this.props.status}</span>;
         break;
     }
   }
